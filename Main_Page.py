718e3683fb5c037446d33a945256c6639bdd5454
Jorrit Bootsma <jorrit@Jorrits-MacBook-Pro.local>
2023-01-04 18:02:47 +0100

    added minimal app descriptioin

--- Main_Page.py
+++ Main_Page.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+st.markdown(
+    """
+    Welcome to the Soccer Stats application. Select your desired page using the sidebar.
+    """
+)
