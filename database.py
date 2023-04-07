d283889ad721582d0054040b1b75ef12d4c02c7b
Jorrit Bootsma <jorrit@Jorrits-MacBook-Pro.local>
2023-04-07 16:03:53 +0200

    added option to connect to non-local database, given local environment variables

--- database.py
+++ database.py
@@ -8,24 +8,35 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 Base = declarative_base()
 
 try:
-    # Database connection specifics for local PostgreSQL connection
+    # Database connection specifics for PostgreSQL connection outside local machine
     server = os.environ["DB_SERVER"]
     user = os.environ["DB_USER"]
-    # password = os.environ["DB_PASSWORD"]
-    # database = os.environ["DB_NAME"]
-    # conn_string = f"postgresql+psycopg2://{user}:{password}@{server}/{database}"
-    conn_string = f"postgresql://{server}/{user}"
-    print(conn_string)
+    password = os.environ["DB_PASSWORD"]
+    database = os.environ["DB_NAME"]
+    conn_string = f"postgresql+psycopg2://{user}:{password}@{server}/{database}"
+    print("Connecting to external database, using: ", conn_string)
 
     # Create engine
     engine = create_engine(conn_string, echo=True)
+
 except KeyError as e:
-    print(
-        "Failed to extract database details environment variables. "
-        "Is this on purpose?"
-    )
-    print(f"Failed on key: {e}")
-    raise
+    try:
+        # Database connection specifics for local PostgreSQL connection
+        server = os.environ["DB_SERVER"]
+        user = os.environ["DB_USER"]
+        conn_string = f"postgresql://{server}/{user}"
+        print("Connection to local database, using: ", conn_string)
+
+        # Create engine
+        engine = create_engine(conn_string, echo=True)
+
+    except KeyError as e:
+        print(
+            "Failed to extract database details environment variables. "
+            "Is this on purpose?"
+        )
+        print(f"Failed on key: {e}")
+        raise
 
 # Initialise session class
 Session = sessionmaker()
